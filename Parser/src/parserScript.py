80b1084904e438404ca8402e383e2958065b88a8
ChehST <chehstream@gmail.com>
2024-04-16 12:02:00 +0400

    PAR-31 add the highest page search foos

--- Parser/src/parserScript.py
+++ Parser/src/parserScript.py
@@ -1,28 +1,31 @@
+#!/usr/local/bin/python3
+
 # Author: Sergey Chekhonin
 # 2024 No rights protected :)
 
 
 from bs4 import BeautifulSoup as BSoup
 
-from utils.toolkit import get_response_bypassed, get_html_f
+from utils.toolkit import get_response_bypassed, get_html_f,\
+    get_page_dict, remove_non_numeric_keys , get_high_link
 
 tartget_url = 'https://www.list.am/category/56/222?n=1&cmtype=1&price2=234567&crc=1&gl=2'
 
-html = get_html_f('Parser/src/fixtures/index.html')
 
-parsed_html = BSoup(html,'lxml')
-paginator = parsed_html.find('div',class_='dlf').find_all('a') # return bs4.element.ResultSet
-for a in paginator:
-    print(a.string)
-    # вытаскивает данные из тега как string
-    # 2
-    # 3
-    # 4
-    # 5
-    # 6
-    # 7
-    # hadjorde >
-    # создать условие чтобы числовые значения с ссылко
-    # сохранял и выбирал ту ссылку значение которой максимальное!
+### PAR-31 Creating funcionality for paginator max page
+### searching
 
-print(type(paginator))
+# 1. Load markup from fixture; 
+html = get_html_f('fixtures/index.html')
+
+# 2. Return soup object to parse new dict
+soup_html = BSoup(html,'lxml')
+
+# 3. Get dict with numerik keys to links
+new_list = get_page_dict(soup_html)
+
+# 4. Clean out alphabethic keys from list
+remove_non_numeric_keys(new_list)
+
+# 5. get the highest link from page
+print(get_high_link(new_list))
--- Parser/src/utils/toolkit.py
+++ Parser/src/utils/toolkit.py
@@ -21,3 +21,32 @@ def get_html_f(file):
     with open(file,'r',encoding="utf-8") as html:
         html_plain = html.read()
     return html_plain
+
+
+###  PAR-31
+# page's link dict
+links = {} # page's link dict
+# Return dict with pages and hrefs ; Input bsoup object
+def get_page_dict(parsed_page):
+    for page in parsed_page.find('div',class_='dlf').find_all('a'):
+        page_num = page.string
+        page_link = page.get('href')
+        # print(page_num, page_link)
+        if page_num and page_link:
+            links[page_num] = page_link
+    return links
+
+# remove non digit key pairs
+def remove_non_numeric_keys(dictionary):
+    keys_to_remove = [key for key in dictionary.keys() if not key.isdigit()]
+    for key in keys_to_remove:
+        del dictionary[key]
+    return links
+
+# get max values link
+def get_highest_link(links):
+    max_key = max(links.keys(), key=lambda x: int(x))
+    max_link = links[max_key]
+
+    return max_link
+
