7bcb9c2278f213f45ab0dceb8e1d62d4b2f0433a
ChehST <chehstream@gmail.com>
2024-04-16 12:00:45 +0400

    PAR-31 add first page link string to consts #comment I think that i've complicating code =(

--- Parser/src/const/__init__.py
+++ Parser/src/const/__init__.py
@@ -1,4 +1,6 @@
-BASE_URL = 'https://www.list.am/category/'
+BASE_URL = 'https://www.list.am/'
+### PAR-31
+FIRST_PAGE_URL = '/category/56/7?n=1&cmtype=1&price2=234567&crc=1&gl=2'
 CATHEGORY = {
     "Realty":"56",
     "Vehicles":"16",
