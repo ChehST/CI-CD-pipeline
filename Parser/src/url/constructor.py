bc9821302a7e06a338b0d7c4e894cc4fbdd6ee45
ChehST <chehstream@gmail.com>
2024-04-17 18:39:31 +0400

    PAR-24 start contributing path constructor but it goes abandone for next release

--- Parser/src/url/constructor.py
+++ Parser/src/url/constructor.py
@@ -5,6 +5,14 @@ on url address
 
 from const import *
 
+target_url_cathPart = BASE_URL + CATHEGORY_LOCK
+target_url_reqPart = "?n=1&cmtype=1&price2=200000&crc=1&gl=2"
+
+def build_path(page_num):
+    target_url_cathPart = BASE_URL + CATHEGORY_LOCK
+    target_url_reqPart = "?n=1&cmtype=1&price2=200000&crc=1&gl=2"
+    req_url = target_url_cathPart + page_num +target_url_reqPart
+    return req_url
 
 # PAR-23
 #      BASE_URL   |   CATHEGORY    |  page_paginator  |  request  |  Filter...
