08c1e0d9fb1097a834128c9f750159ca1fa18f58
ChehST <chehstream@gmail.com>
2024-04-17 18:40:47 +0400

    PAR-19 working tool, that print total ads in cathegory

--- Parser/src/parserScript.py
+++ Parser/src/parserScript.py
@@ -6,7 +6,14 @@
 
 from bs4 import BeautifulSoup as BSoup
 
-from utils.toolkit import count_ads_at_page
+from const import BASE_URL
+from const.codes import CATHEGORY_LOCK
+from utils.toolkit import count_ads_at_page, get_last_page,counter_ads_at_page_request
 
-### PAR-23 Ads per page counter
-count_ads_at_page()
+last_page = get_last_page()
+target_url_cathPart = BASE_URL + CATHEGORY_LOCK
+target_url_reqPart = "?n=1&cmtype=1&price2=200000&crc=1&gl=2"
+ads_last_page = counter_ads_at_page_request(target_url_cathPart + str(last_page) +target_url_reqPart)
+
+total_ads = (last_page * 100) + ads_last_page
+print(f"There are '{total_ads}' rent ads by owners up to 200000 dram in Yerevan area")
