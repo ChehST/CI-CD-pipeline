7c76c61397b0a00a1669badb1b66aff075d9173b
ChehST <chehstream@gmail.com>
2024-05-14 10:56:42 +0400

    fix: relative import errors

--- Parser/src/const/__init__.py
+++ Parser/src/const/__init__.py
@@ -1,5 +1,4 @@
 from .codes import CATHEGORY_LOCK
-
 BASE_URL = 'https://www.list.am/'
 FIRST_PAGE_URL = '/category/56/7?n=1&cmtype=1&price2=234567&crc=1&gl=2'
 CATHEGORY = {
--- Parser/src/parserScript.py
+++ Parser/src/parserScript.py
@@ -6,11 +6,12 @@
 
 from bs4 import BeautifulSoup as BSoup
 
-from .const import BASE_URL
-from .const.codes import CATHEGORY_LOCK
-from .utils.toolkit import count_ads_at_page, get_last_page,counter_ads_at_page_request
+from const import BASE_URL
+from const.codes import CATHEGORY_LOCK
+from utils.toolkit import count_ads_at_page, get_last_page,counter_ads_at_page_request
 
 last_page = get_last_page()
+print(last_page)
 target_url_cathPart = BASE_URL + CATHEGORY_LOCK
 target_url_reqPart = "?n=1&cmtype=1&price2=200000&crc=1&gl=2"
 ads_last_page = counter_ads_at_page_request(target_url_cathPart + str(last_page) +target_url_reqPart)
